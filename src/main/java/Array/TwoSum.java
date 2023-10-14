9a45fd674a996a8f5cffdd5bb697da4e85d5c684
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-10-14 00:40:31 -0500

    1. Two Sum

--- src/main/java/Array/TwoSum.java
+++ src/main/java/Array/TwoSum.java
@@ -1,4 +1,30 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {3,3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+           if(!map.containsKey(target- nums[i])){
+               map.put(nums[i],i);
+           }
+           else{
+               result[0]= map.get(target-nums[i]);
+               result[1]= i;
+               break;
+           }
+        }
+      return  result;
+
+    }
 }
