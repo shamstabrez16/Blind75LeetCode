eec94e11b98dc21cf218cefa95aa85eadf9baac5
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-10-15 00:48:24 -0500

    217 contains Duplicate

---
+++ src/main/java/Array/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[]  nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums
+        ) {
+            if(!set.add(n))
+                return true;
+        }
+        return false;
+    }
+}
