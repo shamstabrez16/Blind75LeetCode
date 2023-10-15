f37357a816c60e613fb75f3598185996a7a37b36
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-10-15 02:29:13 -0500

    238. Product of Array Except Self

---
+++ src/main/java/Array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        int[] nums = {-1,1,0,-3,3};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] pp = new int[nums.length];
+        pp[0]=1;
+        int[] sp = new int[nums.length];
+        sp[nums.length-1]=1;
+        for (int i = 0; i <nums.length-1; i++) {
+            pp[i+1]= pp[i]*nums[i];
+        }
+        for (int i = nums.length-1; i >=1; i--) {
+            sp[i-1]= sp[i]*nums[i];
+            pp[i]= pp[i]*sp[i];
+        }
+        pp[0]= pp[0]*sp[0];
+        return pp;
+    }
+
+}
